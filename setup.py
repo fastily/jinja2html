5e607e78094887bad3556576e2b3ff7a36d5dcb5
fastily <fastily@users.noreply.github.com>
2020-07-23 23:53:17 -0700

    prepare for next dev cycle

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jinja2html",
-    version="0.2.0",
+    version="0.3.0",
     author="Fastily",
     author_email="fastily@users.noreply.github.com",
     description="friendly generation of websites with jinja2 templates",
