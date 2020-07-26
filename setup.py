57357b972f8bbad8f5266b4742bff8edb13aa775
fastily <fastily@users.noreply.github.com>
2020-07-26 03:51:55 -0700

    unbreak module format, tag for release

--- setup.py
+++ setup.py
@@ -5,18 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jinja2html",
-    version="0.3.0",
+    version="0.2.1",
     author="Fastily",
     author_email="fastily@users.noreply.github.com",
     description="friendly generation of websites with jinja2 templates",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/fastily/jinja2html",
+    include_package_data=True,
     packages=setuptools.find_packages(),
     install_requires=['beautifulsoup4', 'Jinja2', 'lxml', 'watchdog', 'websockets'],
     entry_points={
         'console_scripts': [
-            'jinja2html = jinja2html:main'
+            'jinja2html = jinja2html:__main__.main'
         ]
     },
     classifiers=[
@@ -27,4 +28,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.7',
 )
